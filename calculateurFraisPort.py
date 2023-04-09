efd49a18ac48a8e7eb879ce629e0d179e0716caf
EvaDM <evamorante1969@gmail.com>
2023-04-09 14:32:57 +0200

    galèèèère !!!

--- calculateurFraisPort.py
+++ calculateurFraisPort.py
@@ -1,6 +1,24 @@
+user_input_pays = input("Veuillez entrer votre pays : ")
+user_input_montant = input("Veuillez entrer le montant :")
+
 def pays():
-    user_input_pays = "Veuillez entrer votre pays : "
-    return user_input_pays
-    
-print(pays())
+    if user_input_pays == "France":
+        message = "Les frais de port se montent à chf 20."
+    elif user_input_pays == "Allemagne":
+        message = "Les frais de port se montent à chf 25."
+    else:
+        message = "Les frais de port se montent à chf 50."
+    print(message)
+
+pays()
+
+def montant():
+    if user_input_montant >=100:
+        message = "S'ajoutent chf 10."
+    else:
+        message = "S'ajoutent chf 20."
+        print(message)
+
+montant()   
+
 
