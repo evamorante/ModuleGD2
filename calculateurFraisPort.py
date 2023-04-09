a63d983fd49157514a1e82406c68db8f1736a3a6
EvaDM <evamorante1969@gmail.com>
2023-04-09 14:06:20 +0200

    merde !!!

--- calculateurFraisPort.py
+++ calculateurFraisPort.py
@@ -1,24 +1,6 @@
 def pays():
-    pays_input = input("Veuillez indiquer votre pays d'origine :")
-    if pays_input == "France":
-        print ("Les frais de port se montent à CHF 4.90")
-    elif pays_input == "Allemagne":
-        print ("Les frais de port se montent à CHF 5.90")
-    elif pays_input == "Espagne":
-        print ("Les frais de port se montent à CHF 3.90")
-    elif pays_input == "Portugal":
-        print ("Les frais de port se montent à CHF 5.90")
-    else :
-        print ("Les commandes ne sont pas disponibles depuis ce pays")
+    user_input_pays = "Veuillez entrer votre pays : "
+    return user_input_pays
+    
+print(pays())
 
-def coupon():
-    coupon_input = input("Veuillez saisir votre numéro de coupon : ")
-    coupon = "0000"
-    if coupon_input == coupon:
-        input("Vous bénéficiez de 20% pourcent sur votre commande")
-        input("Vous bénéficiez également de la gratuité des frais de port")
-    else:
-        print ("Pas de rabais, coupon non valide")
-
-pays()
-coupon()
