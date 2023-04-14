15df225d86bc701b6d1e78c882ed6144b57285d0
EvaDM <evamorante1969@gmail.com>
2023-04-14 09:11:17 +0200

    practising if/elif/else

---
+++ ifThen.py
@@ -0,0 +1,10 @@
+sun = "sunny"
+sand = "hot"
+
+if sun == "sunny" and sand == "nosand":
+    print("I am in the mountains")
+elif sun == "nosun" or sand == "yellow":
+    print("I am on holiday")
+else:
+    print("I am at home")
+    
