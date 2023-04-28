1ae5bb1dfe09077a45f3b75e1ce5d885f383459f
EvaDM <evamorante1969@gmail.com>
2023-04-28 11:32:51 +0200

    tests on code for the if/then/and/or condition in JS

--- ifThen.js
+++ ifThen.js
@@ -1,9 +1,23 @@
-var sun = "sunny"
-var temp = "cold"
+var sun = "sunny";
+var temp = "cold";
 
 if (sun == "sunny" && temp == "hot"){
     console.log("I am on holidays");
 }
 else{
     console.log("I stay at home");
+
+}
+
+var name = "Emma";
+var number = 15
+
+if (name == "Eva" && number == 12){
+    console.log("Great name & amazing number");
+}
+else if (name == "Eva" || number == 12){
+    console.log("One of both is unbelievable");
+} 
+else {
+    console.log("You have your own values");
 }
