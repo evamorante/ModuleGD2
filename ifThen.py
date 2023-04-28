95178d789d06e1357210c459efb5bc250a82f780
EvaDM <evamorante1969@gmail.com>
2023-04-28 11:22:30 +0200

    tests on code in the if/then/and/or condition in py

--- ifThen.py
+++ ifThen.py
@@ -7,4 +7,14 @@ elif sun == "nosun" or sand == "yellow":
     print("I am on holiday")
 else:
     print("I am at home")
-    
+
+
+name = "Emma"
+number = 15
+
+if name == "Eva" and number == 12:
+    print("Great name & amazing number")
+elif name == "Eva" or number == 12:
+    print("One of both is unbelievable")
+else:
+    print("You got your own values")
